3aaea9a7cae4e40798086332a8267a8646d216d6
Sebastian Bauer <thurion@daily-nerds.de>
2017-08-20 22:41:06 +0200

    Set version to 1.0 Beta

--- load.py
+++ load.py
@@ -42,7 +42,7 @@ import plug
 if __debug__:
     from traceback import print_exc
 
-VERSION = "0.1 Beta"
+VERSION = "1.0 Beta"
 EDSM_UPDATE_INTERVAL = 3600 # 1 hour. used for EliteSystem
 EDSM_NUMBER_OF_SYSTEMS_TO_QUERY = 15
 DEFAULT_UPDATE_INTERVAL = 1
