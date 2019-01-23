ef8d54badd8fd8ba9c1f61e62a708c316f9e0512
Sebastian Bauer <thurion@daily-nerds.de>
2019-01-23 19:22:06 +0100

    Refactoring

--- load.py
+++ load.py
@@ -20,7 +20,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import sys
 import math
 import json
-import re
 import urllib2
 
 from threading import Thread
@@ -39,49 +38,50 @@ import plug
 if __debug__:
     from traceback import print_exc
 
-
-this = sys.modules[__name__]	# For holding module globals
+this = sys.modules[__name__]  # For holding module globals
 
 this.VERSION = "1.1"
+this.VERSION_CHECK_URL = "https://pastebin.com/raw/9sSBrx0W"
+this.versionMismatch = False
+
 this.EDSM_NUMBER_OF_SYSTEMS_TO_QUERY = 15
 
-this.OPTIONS_RADIUS             = lambda x: 39+11*(2**x)
-this.DEFAULT_RADIUS             = 2 # key for radius, see OPTIONS_RADIUS
-this.MAX_RADIUS                 = 10
-this.RADIUS_ADJUSTMENT_INCREASE = 15 # increase radius if at most this amount of systems were found
-this.RADIUS_ADJUSTMENT_DECREASE = 100 # decrease the radius if at least this amount of systems were found
+this.OPTIONS_RADIUS = lambda x: 39 + 11 * (2 ** x)
+this.DEFAULT_RADIUS = 2  # key for radius, see OPTIONS_RADIUS
+this.MAX_RADIUS = 10
+this.RADIUS_ADJUSTMENT_INCREASE = 15  # increase radius if at most this amount of systems were found
+this.RADIUS_ADJUSTMENT_DECREASE = 100  # decrease the radius if at least this amount of systems were found
 
 # Values for projects
 this.PROJECT_RSE = 1
-this.PROJECT_NAVBACON = 2
+this.PROJECT_NAVBEACON = 2
 
 # keys for dictionary that stores data from the background thread
 # stored in this.lastEventInfo
-this.BG_SYSTEM  = "bg_system"
+this.BG_SYSTEM = "bg_system"
 this.BG_MESSAGE = "bg_message"
-
 
 
 class RseHyperlinkLabel(HyperlinkLabel):
 
     def __init__(self, master=None, **kw):
         super(RseHyperlinkLabel, self).__init__(master, **kw)
-        self.menu.add_command(label=_("Ignore"), command = self.ignore)
+        self.menu.add_command(label=_("Ignore"), command=self.ignore)
 
     def ignore(self):
         this.worker.ignore(self["text"])
 
 
 class EliteSystem(object):
-    def __init__(self, id, name, x, y, z, uncertainty = None, action = 0):
-        self.id          = id
-        self.name        = name
-        self.x           = x
-        self.y           = y
-        self.z           = z
+    def __init__(self, id64, name, x, y, z, uncertainty=None, action=0):
+        self.id = id64
+        self.name = name
+        self.x = x
+        self.y = y
+        self.z = z
         self.uncertainty = uncertainty or 0
-        self.distance    = 10000 #set initial value to be out of reach
-        self.action      = action
+        self.distance = 10000  # set initial value to be out of reach
+        self.action = action
         self.action_text = ""
 
     @staticmethod
@@ -101,7 +101,9 @@ class EliteSystem(object):
         return self.calculateDistanceToCoordinates(system2.x, system2.y, system2.z)
 
     def __str__(self):
-        return "id: {id}, name: {name}, distance: {distance:,.2f}, uncertainty: {uncertainty}".format(id=self.id, name=self.name, distance=self.distance, uncertainty=self.uncertainty)
+        return "id: {id}, name: {name}, distance: {distance:,.2f}, uncertainty: {uncertainty}".format(id=self.id, name=self.name,
+                                                                                                      distance=self.distance,
+                                                                                                      uncertainty=self.uncertainty)
 
     def __repr__(self):
         return self.__str__()
@@ -119,19 +121,19 @@ class EliteSystem(object):
 
 
 class BackgroundWorker(Thread):
-    
     # instructions. don't use 0!
     JUMPED_SYSTEM = 1
     NAVBEACON = 2
 
-    def __init__(self, queue, radius = this.DEFAULT_RADIUS):
+    def __init__(self, queue, radius=this.DEFAULT_RADIUS):
         Thread.__init__(self)
         self.queue = queue
         self.radius = radius
-        self.systemList = list() # nearby systems, sorted by distance
+        self.systemList = list()  # nearby systems, sorted by distance
         self.projectsDict = dict()
-        self.filter = set() # systems that have been completed
-
+        self.filter = set()  # systems that have been completed
+        self.c = None
+        self.conn = None
 
     def ignore(self, systemName):
         for system in self.systemList:
@@ -139,8 +141,7 @@ class BackgroundWorker(Thread):
                 system.action = 0
                 self.removeSystems()
                 self.showNewClosestSystem()
-                break;
-
+                break
 
     def adjustRadius(self, numberOfSystems):
         if numberOfSystems <= this.RADIUS_ADJUSTMENT_INCREASE:
@@ -154,36 +155,33 @@ class BackgroundWorker(Thread):
                 self.radius = 0
             if __debug__: print("found {0} systems, decreasing radius to {1}".format(numberOfSystems, this.OPTIONS_RADIUS(self.radius)))
 
-
     def openDatabase(self):
         try:
-            self.conn = psycopg2.connect(host="cyberlord.de", port=5432, dbname="edmc_rse_db", user="edmc_rse_user", 
-                                         password="asdfplkjiouw3875948zksmdxnf", application_name="EDSM-RSE {}".format(this.VERSION), connect_timeout=10)
+            self.conn = psycopg2.connect(host="cyberlord.de", port=5432, dbname="edmc_rse_db", user="edmc_rse_user",
+                                         password="asdfplkjiouw3875948zksmdxnf", application_name="EDSM-RSE {}".format(this.VERSION),
+                                         connect_timeout=10)
             self.c = self.conn.cursor()
         except Exception as e:
             plug.show_error("EDSM-RSE: Database could not be opened")
             sys.stderr.write("EDSM-RSE: Database could not be opened\n")
 
-
     def closeDatabase(self):
         if not hasattr(self, "c") or not self.c:
-            return # database not loaded
+            return  # database not loaded
         self.conn.close()
         self.c = None
         self.conn = None
 
-
     def initializeDictionaries(self):
         if not hasattr(self, "c") or not self.c:
-            return # database not loaded
+            return  # database not loaded
 
         if len(self.projectsDict) == 0:
             self.c.execute("SELECT id,action_text FROM projects")
             self.projectsDict = dict()
-            for row in self.c.fetchall():
-                id, action_text = row
-                self.projectsDict[id] = action_text
-
+            for _row in self.c.fetchall():
+                _id, action_text = _row
+                self.projectsDict[_id] = action_text
 
     def generateListsFromDatabase(self, x, y, z):
         sql = " ".join([
@@ -203,30 +201,30 @@ class BackgroundWorker(Thread):
             "z1": z - this.OPTIONS_RADIUS(self.radius),
             "z2": z + this.OPTIONS_RADIUS(self.radius)
         })
-        for row in self.c.fetchall():
-            id, name, x2, y2, z2, uncertainty, action = row
+        for _row in self.c.fetchall():
+            _, name, x2, y2, z2, uncertainty, action = _row
             distance = EliteSystem.calculateDistance(x, x2, y, y2, z, z2)
             if distance <= this.OPTIONS_RADIUS(self.radius):
-                eliteSystem = EliteSystem(*row)
+                eliteSystem = EliteSystem(*_row)
                 eliteSystem.distance = distance
-                eliteSystem.action_text = ", ".join([self.projectsDict[project] for project in self.projectsDict.keys() if (eliteSystem.action & project) == project])
+                eliteSystem.action_text = ", ".join(
+                    [self.projectsDict[project] for project in self.projectsDict.keys() if (eliteSystem.action & project) == project])
                 systems.append(eliteSystem)
- 
+
         # filter out systems that have been completed
-        systems = filter(lambda system: system.id not in self.filter, systems)        
+        systems = filter(lambda system: system.id not in self.filter, systems)
         systems.sort(key=lambda l: l.distance)
 
         self.systemList = systems
         self.adjustRadius(len(self.systemList))
 
-
     def removeSystems(self):
         removeMe = filter(lambda x: x.action == 0, self.systemList)
-        if __debug__: print("adding {count} systems to removal filter: {systems}".format(count=len(removeMe), systems=[x.name for x in removeMe]))
+        if __debug__: print(
+            "adding {count} systems to removal filter: {systems}".format(count=len(removeMe), systems=[x.name for x in removeMe]))
         self.systemList = [x for x in self.systemList if x not in removeMe]
         for system in removeMe:
             self.filter.add(system.id)
-
 
     def queryEDSM(self, systems):
         # TODO: use a cache
@@ -249,18 +247,17 @@ class BackgroundWorker(Thread):
                     names.add(entry["name"].lower())
                 return names
             except:
-               # ignore. the EDSM call is not required
-               if __debug__: print_exc()
+                # ignore. the EDSM call is not required
+                if __debug__: print_exc()
         return set()
 
-
-    def getSystemFromID(self, id):
-        system = filter(lambda x: x.id == id, self.systemList)[:1] # there is only one possible match for ID64, avoid exception being thrown
+    def getSystemFromID(self, id64):
+        system = filter(lambda x: x.id == id64, self.systemList)[
+                 :1]  # there is only one possible match for ID64, avoid exception being thrown
         if len(system) > 0:
             return system[0]
         else:
             return None
-
 
     def showNewClosestSystem(self):
         this.lastEventInfo = dict()
@@ -268,25 +265,23 @@ class BackgroundWorker(Thread):
             this.lastEventInfo[this.BG_SYSTEM] = self.systemList[0]
         else:
             this.lastEventInfo[this.BG_MESSAGE] = "No system in range"
-        this.frame.event_generate("<<EDSM-RSE_BackgroundWorker>>", when="tail") # calls updateUI in main thread
-
+        this.frame.event_generate("<<EDSM-RSE_BackgroundWorker>>", when="tail")  # calls updateUI in main thread
 
     def handleJumpedSystem(self, coordinates, systemAddress):
         system = self.getSystemFromID(systemAddress)
 
-        if system: # arrived in system without coordinates
+        if system:  # arrived in system without coordinates
             if __debug__: print("arrived in {}".format(system.name))
             system.removeFromProject(this.PROJECT_RSE)
             self.removeSystems()
 
-        if hasattr(self, "c") and self.c: # make sure the database is accessible
+        if hasattr(self, "c") and self.c:  # make sure the database is accessible
             self.generateListsFromDatabase(*coordinates)
             lowerLimit = 0
             upperLimit = this.EDSM_NUMBER_OF_SYSTEMS_TO_QUERY
-            
-            closestSystems = list()
+
             tries = 0
-            while tries < 3 and len(self.systemList) > 0: # no do-while loops...
+            while tries < 3 and len(self.systemList) > 0:  # no do-while loops...
                 closestSystems = self.systemList[lowerLimit:upperLimit]
                 edsmResults = self.queryEDSM(closestSystems)
                 if len(edsmResults) > 0:
@@ -303,7 +298,7 @@ class BackgroundWorker(Thread):
                     tries += 1
                     lowerLimit += this.EDSM_NUMBER_OF_SYSTEMS_TO_QUERY
                     upperLimit += this.EDSM_NUMBER_OF_SYSTEMS_TO_QUERY
-            
+
             self.showNewClosestSystem()
 
         else:
@@ -312,14 +307,12 @@ class BackgroundWorker(Thread):
                 system.updateDistanceToCurrentCommanderPosition(*coordinates)
             self.systemList.sort(key=lambda l: l.distance)
 
-
     def handleNavbeacon(self, systemAddress):
         system = self.getSystemFromID(systemAddress)
         if system:
-            system.removeFromProject(this.PROJECT_NAVBACON)
+            system.removeFromProject(this.PROJECT_NAVBEACON)
             self.removeSystems()
             self.showNewClosestSystem()
-
 
     def run(self):
         self.openDatabase()
@@ -335,7 +328,7 @@ class BackgroundWorker(Thread):
                 self.handleJumpedSystem(*args)
                 self.closeDatabase()
             elif instruction == self.NAVBEACON:
-                self.handleNavbeacon(args) # args is only 1 ID64
+                self.handleNavbeacon(args)  # args is only 1 ID64
             self.queue.task_done()
         self.closeDatabase()
         self.queue.task_done()
@@ -347,8 +340,12 @@ def checkTransmissionOptions():
     return eddn or edsm
 
 
+def versionCheck():
+    pass
+
+
 def plugin_start():
-    settings = config.getint("EDSM-RSE") or 5 # default setting: radius 0 is currently not selectable
+    settings = config.getint("EDSM-RSE") or 5  # default setting: radius 0 is currently not selectable
     this.clipboard = tk.IntVar(value=((settings >> 5) & 0x01))
     this.overwrite = tk.IntVar(value=((settings >> 6) & 0x01))
 
@@ -364,7 +361,7 @@ def plugin_start():
     return "EDSM-RSE"
 
 
-def updateUI(event = None):
+def updateUI(event=None):
     eliteSystem = this.lastEventInfo.get(this.BG_SYSTEM, None)
     message = this.lastEventInfo.get(this.BG_MESSAGE, None)
     if (this.enabled or this.overwrite.get()) and eliteSystem:
@@ -375,7 +372,7 @@ def updateUI(event = None):
         this.unconfirmedSystem["state"] = "enabled"
         distanceText = u"{distance} Ly".format(distance=Locale.stringFromNumber(eliteSystem.distance, 2))
         if eliteSystem.uncertainty > 0:
-            distanceText = distanceText +  u" (\u00B1{uncertainty})".format(uncertainty=eliteSystem.uncertainty)
+            distanceText = distanceText + u" (\u00B1{uncertainty})".format(uncertainty=eliteSystem.uncertainty)
         this.distanceValue["text"] = distanceText
         this.actionText["text"] = eliteSystem.action_text
         if this.clipboard.get():
@@ -403,6 +400,7 @@ def plugin_prefs(parent):
     PADX = 5
     global row
     row = 0
+
     def nextRow():
         global row
         row += 1
@@ -412,13 +410,17 @@ def plugin_prefs(parent):
     frame.columnconfigure(0, weight=1)
 
     row = 0
-    nb.Checkbutton(frame, variable=this.clipboard, text="Copy system name to clipboard after jump").grid(row=nextRow(), column=0, columnspan=2, padx=PADX, sticky=tk.W)
-    nb.Checkbutton(frame, variable=this.overwrite, text="I use another tool to transmit data to EDSM/EDDN").grid(row=nextRow(), column=0, columnspan=2, padx=PADX, sticky=tk.W)
+    nb.Checkbutton(frame, variable=this.clipboard,
+                   text="Copy system name to clipboard after jump").grid(row=nextRow(), column=0, columnspan=2, padx=PADX, sticky=tk.W)
+    nb.Checkbutton(frame, variable=this.overwrite,
+                   text="I use another tool to transmit data to EDSM/EDDN").grid(row=nextRow(), column=0, columnspan=2, padx=PADX, sticky=tk.W)
 
-    ttk.Separator(frame, orient=tk.HORIZONTAL).grid(row=nextRow(), columnspan=2, padx=PADX*2, pady=8, sticky=tk.EW)
+    ttk.Separator(frame, orient=tk.HORIZONTAL).grid(row=nextRow(), columnspan=2, padx=PADX * 2, pady=8, sticky=tk.EW)
     nb.Label(frame, text="Plugin Version: {}".format(this.VERSION)).grid(row=nextRow(), column=0, columnspan=2, padx=PADX, sticky=tk.W)
-    HyperlinkLabel(frame, text="Open the Github page for this plugin", background=nb.Label().cget("background"), url="https://github.com/Thurion/EDSM-RSE-for-EDMC", underline=True).grid(row=nextRow(), column=0, columnspan=2, padx=PADX, sticky=tk.W)
-    HyperlinkLabel(frame, text="A big thanks to EDTS for providing the coordinates.", background=nb.Label().cget("background"), url="http://edts.thargoid.space/", underline=True).grid(row=nextRow(), column=0, columnspan=2, padx=PADX, sticky=tk.W)
+    HyperlinkLabel(frame, text="Open the Github page for this plugin", background=nb.Label().cget("background"),
+                   url="https://github.com/Thurion/EDSM-RSE-for-EDMC", underline=True).grid(row=nextRow(), column=0, columnspan=2, padx=PADX, sticky=tk.W)
+    HyperlinkLabel(frame, text="A big thanks to EDTS for providing the coordinates.", background=nb.Label().cget("background"),
+                   url="http://edts.thargoid.space/", underline=True).grid(row=nextRow(), column=0, columnspan=2, padx=PADX, sticky=tk.W)
     return frame
 
 
@@ -441,8 +443,8 @@ def plugin_app(parent):
     this.frame.bind_all("<<EDSM-RSE_BackgroundWorker>>", updateUI)
     this.frame.columnconfigure(1, weight=1)
     tk.Label(this.frame, text="Unconfirmed:").grid(row=0, column=0, sticky=tk.W)
-    this.unconfirmedSystem = RseHyperlinkLabel(frame, compound=tk.RIGHT, popup_copy = True)
-    this.errorLabel = tk.Label(frame)
+    this.unconfirmedSystem = RseHyperlinkLabel(this.frame, compound=tk.RIGHT, popup_copy=True)
+    this.errorLabel = tk.Label(this.frame)
     tk.Label(this.frame, text="Distance:").grid(row=1, column=0, sticky=tk.W)
     this.distanceValue = tk.Label(this.frame)
     this.distanceValue.grid(row=1, column=1, sticky=tk.W)
@@ -452,12 +454,12 @@ def plugin_app(parent):
     this.actionText.grid(row=2, column=1, sticky=tk.W)
 
     updateUI()
-    return frame
+    return this.frame
 
 
 def journal_entry(cmdr, is_beta, system, station, entry, state):
     if not this.enabled and not this.overwrite.get() or is_beta:
-        return # nothing to do here
+        return  # nothing to do here
     if entry["event"] == "FSDJump" or entry["event"] == "Location":
         if "StarPos" in entry:
             this.queue.put((BackgroundWorker.JUMPED_SYSTEM, (tuple(entry["StarPos"]), entry["SystemAddress"])))
