25e44a94a6635719f6350fe8f618d1f751a93918
Sebastian Bauer <thurion@daily-nerds.de>
2020-06-23 04:03:45 +0200

    Retain list of systems if there isn't a new one

--- BackgroundTask.py
+++ BackgroundTask.py
@@ -100,7 +100,7 @@ class NavbeaconTask(BackgroundTaskClosestSystem):
 class JumpedSystemTask(BackgroundTaskClosestSystem):
     def __init__(self, rseData, eliteSystem):
         super(JumpedSystemTask, self).__init__(rseData)
-        self.coordinates = (eliteSystem.x, eliteSystem.y, eliteSystem.z)
+        self.coordinates = eliteSystem.getCoordinates()
         self.systemAddress = eliteSystem.id64
 
     def queryEDSM(self, systems):
@@ -145,36 +145,38 @@ class JumpedSystemTask(BackgroundTaskClosestSystem):
             system.removeFromProject(RseData.PROJECT_RSE)
             self.removeSystems()
 
-        if self.rseData.generateListsFromRemoteDatabase(*self.coordinates):
-            lowerLimit = 0
-            upperLimit = RseData.EDSM_NUMBER_OF_SYSTEMS_TO_QUERY
-
-            tries = 0
-            while tries < 3 and len(self.rseData.systemList) > 0:  # no do-while loops...
-                closestSystems = self.rseData.systemList[lowerLimit:upperLimit]
-                edsmResults = self.queryEDSM(closestSystems)
-                if len(edsmResults) > 0:
-                    # remove systems with coordinates
-                    systemsWithCoordinates = filter(lambda s: s.name.lower() not in edsmResults, closestSystems)
-                    for system in systemsWithCoordinates:
-                        system.removeFromProject(RseData.PROJECT_RSE)
-                    self.removeSystems()
-                    closestSystems = list(filter(lambda s: s.name.lower() in edsmResults, closestSystems))
-                if len(closestSystems) > 0:
-                    # there are still systems in the results -> stop here
-                    break
-                else:
-                    tries += 1
-                    lowerLimit += RseData.EDSM_NUMBER_OF_SYSTEMS_TO_QUERY
-                    upperLimit += RseData.EDSM_NUMBER_OF_SYSTEMS_TO_QUERY
-
-        else:
+        if not self.rseData.generateListsFromRemoteDatabase(*self.coordinates):
             # distances need to be recalculated because we couldn't get a new list from the database
+            if __debug__: print("EDSM-RSE: Using cached system list for distances. Radius is set to {}".format(self.rseData.calculateRadius()))
             for system in self.rseData.systemList:
                 system.updateDistanceToCurrentCommanderPosition(*self.coordinates)
             self.rseData.systemList.sort(key=lambda l: l.distance)
+        else:
+            # got systems from the database -> need to adjust radius
+            self.rseData.adjustRadiusExponent(len(self.rseData.systemList))
 
-        self.rseData.adjustRadiusExponent(len(self.rseData.systemList))
+        lowerLimit = 0
+        upperLimit = RseData.EDSM_NUMBER_OF_SYSTEMS_TO_QUERY
+
+        tries = 0
+        while tries < 3 and len(self.rseData.systemList) > 0:  # no do-while loops...
+            closestSystems = self.rseData.systemList[lowerLimit:upperLimit]
+            edsmResults = self.queryEDSM(closestSystems)
+            if len(edsmResults) > 0:
+                # remove systems with coordinates
+                systemsWithCoordinates = filter(lambda s: s.name.lower() not in edsmResults, closestSystems)
+                for system in systemsWithCoordinates:
+                    system.removeFromProject(RseData.PROJECT_RSE)
+                self.removeSystems()
+                closestSystems = list(filter(lambda s: s.name.lower() in edsmResults, closestSystems))
+            if len(closestSystems) > 0:
+                # there are still systems in the results -> stop here
+                break
+            else:
+                tries += 1
+                lowerLimit += RseData.EDSM_NUMBER_OF_SYSTEMS_TO_QUERY
+                upperLimit += RseData.EDSM_NUMBER_OF_SYSTEMS_TO_QUERY
+
         self.fireEvent()
 
 
